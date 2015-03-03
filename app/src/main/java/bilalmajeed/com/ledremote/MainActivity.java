b051618d04edd4aa910371ada53d00ad11026f16
Bilal Majeed <bilalmajeed247@gmail.com>
2015-03-02 22:46:27 -0500

    added error handling

--- app/src/main/java/bilalmajeed/com/ledremote/MainActivity.java
+++ app/src/main/java/bilalmajeed/com/ledremote/MainActivity.java
@@ -1,10 +1,13 @@
 package bilalmajeed.com.ledremote;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -14,6 +17,8 @@ import android.content.Intent;
 
 import java.io.IOException;
 import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.util.Set;
 import java.util.UUID;
 
@@ -24,8 +29,9 @@ public class MainActivity extends Activity {
     Button offButton;
     Button connectButton;
 
-    boolean error = false;
-    String remoteDeviceName = "HC-06";
+    private final String remoteDeviceName = "HC-06";
+    private final String BUTTON_PRESS_ERROR = "The phone must be connected to the device";
+    private String CONNECT_BUTTON_ERROR;
 
     private BluetoothAdapter btAdapter;
     private BluetoothSocket btSocket;
@@ -45,66 +51,74 @@ public class MainActivity extends Activity {
         btAdapter = BluetoothAdapter.getDefaultAdapter();
 
         if(btAdapter == null){
-            Toast.makeText(this, "Your device is not supported", Toast.LENGTH_LONG).show();
+            messageBox("DEVICE NOT SUPPORTED", "Your device does not have bluetooth capabilities");
         }
         if(!btAdapter.isEnabled()){
-            Toast.makeText(this, "Bluetooth must be enabled", Toast.LENGTH_LONG).show();
             Intent turnBTOn = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
             startActivityForResult(turnBTOn, 1);
         }
+
         buttonClickListener();
     }
 
     private void buttonClickListener() {
+
+        /* CONNECT BUTTON LISTENER */
         connectButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View connectClick) {
                 findRemoteDevice();
                 try{
                     connectRemoteDevice();
-                }catch(IOException e){
-                    error = true;
+                }catch(Exception e){
+                    if(!btAdapter.isEnabled())
+                        CONNECT_BUTTON_ERROR = "Bluetooth is not enabled on the phone";
+                    else if(btSocket.isConnected())
+                        CONNECT_BUTTON_ERROR = "Already connected to " + remoteDeviceName;
+                    else if(!btSocket.isConnected())
+                        CONNECT_BUTTON_ERROR = "Device: " + btRemoteDevice.getName() + " not found";
+                    else
+                        CONNECT_BUTTON_ERROR = "Unidentified ERROR";
+
+                    messageBox("CONNECT Button", CONNECT_BUTTON_ERROR);
                 }
             }
         });
 
+        /* ON BUTTON LISTENER */
         onButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View onClick) {
                 try{
                     turnON();
-                }catch(IOException e){
-                    error = true;
+                }catch(Exception e){
+                    messageBox("ON Button", BUTTON_PRESS_ERROR);
                 }
             }
         });
 
+        /* OFF BUTTON LISTENER */
         offButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View offClick) {
                 try{
                     turnOFF();
-                }catch(IOException e){
-                    e.getMessage();
+                }catch(Exception e){
+                    messageBox("OFF Button", BUTTON_PRESS_ERROR);
                 }
             }
         });
-
-        if (error) {
-            Toast.makeText(this, "error", Toast.LENGTH_LONG).show();
-        }
     }
 
     public void findRemoteDevice(){
         Set<BluetoothDevice> pairedDevices = btAdapter.getBondedDevices();
 
         //if there any paired devices
-        if(pairedDevices.size() > 0){
+        if(pairedDevices.size() > 0) {
             //loop through paired devices
-            for (BluetoothDevice deviceFound : pairedDevices){
-                if(deviceFound.getName().equals(remoteDeviceName)){
+            for (BluetoothDevice deviceFound : pairedDevices) {
+                if (deviceFound.getName().equals(remoteDeviceName))
                     btRemoteDevice = deviceFound;
-                }
             }
         }
     }
@@ -115,6 +129,9 @@ public class MainActivity extends Activity {
         btSocket.connect();
         output = btSocket.getOutputStream();
         Toast.makeText(this, "Connected", Toast.LENGTH_LONG).show();
+        connectButton.setClickable(false);
+        connectButton.setEnabled(false);
+        connectButton.setText(getResources().getString(R.string.connectedText));
     }
 
     public void turnON() throws IOException{
@@ -127,6 +144,17 @@ public class MainActivity extends Activity {
         String msg = "0";
         msg += "\n";
         output.write(msg.getBytes());
+    }
+
+    private void messageBox(String method, String message) {
+        Log.d("EXCEPTION: " + method, message);
+
+        AlertDialog.Builder messageBox = new AlertDialog.Builder(this);
+        messageBox.setTitle(method);
+        messageBox.setMessage(message);
+        messageBox.setCancelable(false);
+        messageBox.setNeutralButton("OK", null);
+        messageBox.show();
     }
 
     @Override
