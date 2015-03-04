7e4372c1f205bafe70ef9da7fd1fb980d9f24a74
Bilal Majeed <bilalmajeed247@gmail.com>
2015-03-03 19:59:43 -0500

    added a few comments

--- app/src/main/java/bilalmajeed/com/ledremote/MainActivity.java
+++ app/src/main/java/bilalmajeed/com/ledremote/MainActivity.java
@@ -5,7 +5,6 @@ import android.app.AlertDialog;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
-import android.content.res.Resources;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -17,8 +16,6 @@ import android.content.Intent;
 
 import java.io.IOException;
 import java.io.OutputStream;
-import java.io.PrintWriter;
-import java.io.StringWriter;
 import java.util.Set;
 import java.util.UUID;
 
@@ -31,9 +28,9 @@ public class MainActivity extends Activity {
     Button connectButton;
 
     //declare a few constant error messages
-    private final String remoteDeviceName = "HC-06";
+    private final String deviceName = "HC-06";
     private final String CONNECTION_ERROR= "The phone must be connected to the device";
-    private final String PAIRING_ERROR = "Device: " + remoteDeviceName + " not paired";
+    private final String PAIRING_ERROR = "Device: " + deviceName + " not paired";
     private String CONNECT_BUTTON_ERROR = "Unidentified ERROR";
 
     //declare the objects needed for the bluetooth
@@ -44,46 +41,53 @@ public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //initialize the buttons
         onButton = (Button) findViewById(R.id.onButton);
         offButton = (Button) findViewById(R.id.offButton);
         connectButton = (Button) findViewById(R.id.connectButton);
 
+        //initialize the btAdapter
         btAdapter = BluetoothAdapter.getDefaultAdapter();
 
+        //if there not btAdapter, meaning no Bluetooth support on the phone. Then notify user
         if(btAdapter == null){
             messageBox("DEVICE NOT SUPPORTED", "Your device does not have bluetooth capabilities");
         }
+
+        //if the bluetooth is not enabled then ask for the user to enable it
         if(!btAdapter.isEnabled()){
             Intent turnBTOn = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
             startActivityForResult(turnBTOn, 1);
         }
-
     }
-
+    //IF CONNECT BUTTON CLICKED
     public void connect(View view){
-
+        //find the paired device
         findRemoteDevice();
 
         try {
+            //connect to the remote device
             connectRemoteDevice();
         } catch (IOException e) {
+            //handle any error
             if(!btAdapter.isEnabled())
                 CONNECT_BUTTON_ERROR = "Bluetooth is not enabled on the phone";
             else if(btSocket.isConnected())
-                CONNECT_BUTTON_ERROR = "Already connected to " + remoteDeviceName;
+                CONNECT_BUTTON_ERROR = "Already connected to " + deviceName;
             else if(!btSocket.isConnected())
                 CONNECT_BUTTON_ERROR = "Device: " + btRemoteDevice.getName() + " not found";
             else
                 CONNECT_BUTTON_ERROR = "Unidentified ERROR";
 
+            //show a message to the user with the error
             messageBox("CONNECT Button", CONNECT_BUTTON_ERROR);
         }
     }
 
+    //IF ON BUTTON CLICKED
     public void turnON(View view){
         try{
             output.write("1\n".getBytes());
@@ -92,6 +96,7 @@ public class MainActivity extends Activity {
         }
     }
 
+    //IF OFF BUTTON CLICKED
     public void turnOFF(View view){
         try{
             output.write("0\n".getBytes());
@@ -107,23 +112,30 @@ public class MainActivity extends Activity {
         if(pairedDevices.size() > 0) {
             //loop through paired devices
             for (BluetoothDevice deviceFound : pairedDevices) {
-                if (deviceFound.getName().equals(remoteDeviceName))
+                if (deviceFound.getName().equals(deviceName))
                     btRemoteDevice = deviceFound;
             }
         }
     }
 
     public void connectRemoteDevice() throws IOException{
+        //set uuid
         UUID btUUID = UUID.fromString("00001101-0000-1000-8000-00805f9b34fb");
+
+        //create a rfcomm socket and connect tot device
         btSocket = btRemoteDevice.createRfcommSocketToServiceRecord(btUUID);
         btSocket.connect();
         output = btSocket.getOutputStream();
+
         Toast.makeText(this, "Connected", Toast.LENGTH_LONG).show();
+
+        //disable the connect button, so you can not click to it once connected
         connectButton.setClickable(false);
         connectButton.setEnabled(false);
         connectButton.setText(getResources().getString(R.string.connectedText));
     }
 
+    //disables a message box to the user with the inputed message with one button
     private void messageBox(String method, String message) {
         Log.d("EXCEPTION: " + method, message);
 
